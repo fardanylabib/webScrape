38ad2aeddfd24c2bc75b21ce665210fd04f4174a
Labib Fardany <fardanylabib@gmail.com>
2018-04-01 19:40:24 +0700

    displaying header names

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const puppeteer = require('puppeteer');
+const userAgent = require('random-useragent');
 
 const scrape = async () => {
   // Actual Scraping goes Here...
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false, executablePath : 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe' });
   const page = await browser.newPage();
-  page.setExtraHTTPHeaders({ 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' });
-  await page.goto('https://www.upwork.com/o/profiles/browse/?loc=indonesia', { timeout: 0 });
+  const agent = userAgent.getRandom();
+  console.log(agent);
+  page.setExtraHTTPHeaders({ 'user-agent': agent});
+  await page.goto('https://www.upwork.com/o/profiles/browse/?loc=indonesia', { timeout: 0 , waitUntil : 'domcontentloaded'});
+  console.log('sini1');
   const result = await page.evaluate(()=>{
   	let data = [];
     let elements = document.querySelectorAll('a.freelancer-tile-name')
-    
+    console.log('sini2');
   	for(var element of elements){
   		data.push(element.href);
     }    
@@ -17,8 +21,14 @@ const scrape = async () => {
   	return data;
   });
 
-  console.log(result);
-  page.goto(result[0]);
+  for (const link of result) {
+  	  await page.goto(link,{ timeout: 0 , waitUntil : 'domcontentloaded'});
+  	  const res = await page.evaluate(() => {
+  	  	return document.title
+  	  })
+
+  	  console.log(res)
+  }
 
   browser.close(); 
   // Return a value
@@ -26,9 +36,10 @@ const scrape = async () => {
 };
 
 scrape().then(async (links) => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  page.setExtraHTTPHeaders({ 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' });
+	console.log('sini4');
+  // const browser = await puppeteer.launch({ headless: false });
+  // const page = await browser.newPage();
+  // page.setExtraHTTPHeaders({ 'user-agent': userAgent.getRandom()});
 
-  await page.goto(links[0], { timeout: 0 });
+  // await page.goto(links[0], { timeout: 0 });
 });
