5f609dd22f47129b123da1ca68462bf5379d9dc7
Labib Fardany <fardanylabib@gmail.com>
2018-04-01 14:40:51 +0700

    using puppeteer

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
-// var xray = require('x-ray')()
-// const makeDriver = require('request-x-ray')
+const puppeteer = require('puppeteer');
 
+let scrape = async () => {
+  // Actual Scraping goes Here...
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://www.upwork.com/o/profiles/browse/?loc=indonesia');
+  
+  const result = await page.evaluate(()=>{
+  	let data = [];
+  	let elements = document.querySelectorAll('a.freelancer-tile-name')
+  	
+  	for(var element of elements){
+  		data.push(element);
+  	}
+  	return data;
+  });
 
+  browser.close(); 
+  // Return a value
+   return result;
+};
 
-var url = 'https://www.upwork.com/o/profiles/browse/?loc=indonesia'
-var headers = {
-	'user-agent': 'siap'
-}
-
-async function getLinks(url) {
-	var nightmare = require('nightmare')({ show: true })
-	var result = await nightmare
-					.goto(url, headers)
-					// .wait('.air-card-hover.air-card-hover_tile')
-					.evaluate(() => {
-						nodeList = document.querySelectorAll('a.freelancer-tile-name')
-						links = []
-						nodeList.forEach(value => { links.push(value.href)})
-
-						return links 
-					})
-					.end()
-				
-	return result
-}
-
-async function process(url) {
-	var links = await getLinks(url)
-	// console.log(nightmare)
-	links.map(async link => {
-		var nightmare = require('nightmare')({ show: true })
-		console.log(link)
-		await nightmare.goto(link, headers).evaluate(() => {
-			text = document.querySelector('.m-xs-bottom span span').textContent
-			return text
-		}).end().then(console.log)
-	})
-	
-}
-
-// iterate
-process(url)
-
-
+scrape().then((value) => {
+    console.log(value); // Success!
+});
